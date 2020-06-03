2779e15852fb7f9dc3e6f6361087088609e640f3
Dvir <dvir019@gmail.com>
2020-06-03 20:57:13 +0300

    Add documentation to MarkovOne class in part1

--- src/part1/MarkovOne.java
+++ src/part1/MarkovOne.java
@@ -3,11 +3,18 @@ package part1;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * Generates random text basing on the last chosen character
+ */
 public class MarkovOne {
     private String myText;
     private Random myRandom;
 
+    private static final int ONE = 1;
 
+    /**
+     * Initializes myRandom
+     */
     public MarkovOne() {
         myRandom = new Random();
     }
@@ -16,17 +23,31 @@ public class MarkovOne {
         myRandom = new Random(seed);
     }
 
+    /**
+     * Sets myText to the given string after trimming it  # TODO improve
+     *
+     * @param s The new training text
+     */
     public void setTraining(String s) {
         myText = s.trim();
     }
 
+    /**
+     * Chooses random text contains numChars characters
+     * <p>
+     * Chooses randomly one character from the text, and then repeatedly choose one more
+     * character from the list of the characters that appears after the last chosen character.
+     *
+     * @param numChars The number of characters to choose from the text
+     * @return The randomly chosen characters
+     */
     public String getRandomText(int numChars) {
         if (myText == null) {
             return "";
         }
-        StringBuilder sb = new StringBuilder();
 
-        int index = myRandom.nextInt(myText.length() - 1);
+        StringBuilder sb = new StringBuilder();
+        int index = myRandom.nextInt(myText.length() - ONE);
         char selectedChar = myText.charAt(index);
         sb.append(selectedChar);
 
@@ -44,16 +65,27 @@ public class MarkovOne {
         return sb.toString();
     }
 
+    /**
+     * Gets a list of all characters which appears after the given key
+     * <p>
+     * Iterates over the training text and adds all of the characters that appear
+     * directly after the given key into the follows list, and returns the list.
+     * <p>
+     * If there are no characters after the key, an empty list will be returned.
+     *
+     * @param key The # TODO add description
+     * @return A list of all of the characters which appear directly after the key
+     */
     public ArrayList<Character> getFollows(String key) {
         ArrayList<Character> follows = new ArrayList<>();
         int startIndex = myText.indexOf(key);
 
-        while (startIndex != -1) {
+        while (startIndex != -ONE) {
             int followingCharIndex = startIndex + key.length();
             if (followingCharIndex < myText.length()) {
                 follows.add(myText.charAt(followingCharIndex));
             }
-            startIndex = myText.indexOf(key, startIndex + 1);
+            startIndex = myText.indexOf(key, startIndex + ONE);
         }
         return follows;
     }
