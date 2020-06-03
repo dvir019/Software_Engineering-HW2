a703cccc370f6487be81db50ad426933bf75d47e
Dvir <dvir019@gmail.com>
2020-06-03 20:33:53 +0300

    Add documentation to MarkovZero class in part1

--- src/part1/MarkovZero.java
+++ src/part1/MarkovZero.java
@@ -2,10 +2,12 @@ package part1;
 
 import java.util.Random;
 
+/** Generates random text without basing on previous characters */
 public class MarkovZero {
     private String myText;
     private Random myRandom;
 
+    /** Initializes myRandom */
     public MarkovZero() {
         myRandom = new Random();
     }
@@ -14,10 +16,24 @@ public class MarkovZero {
         myRandom = new Random(seed);
     }
 
+    /**
+     * Sets myText to the given string after trimming it  # TODO improve
+     *
+     * @param s The new training text
+     */
     public void setTraining(String s) {
         myText = s.trim();
     }
 
+    /**
+     * Chooses random text contains numChars characters
+     *
+     * Chooses randomly a given number of characters from the training text,
+     * and returns the chosen characters as a string.
+     *
+     * @param numChars The number of characters to choose from the text
+     * @return The randomly chosen characters
+     */
     public String getRandomText(int numChars) {
         if (myText == null) {
             return "";
